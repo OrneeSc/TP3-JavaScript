add552ef9964f77d9d23474225e96d5c76530d9a
Miranda Lopez <mirandaineslopez@gmail.com>
2020-10-09 22:06:01 +0200

    aporte de Miri 2

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
-const showDataTable = ({name}) => {
+const showDataTable = (name, email, address, phone) => {
     const tBody = document.querySelector("#main-tabla_tbody");
-    const td = document.createElement(td);
+    const tr = document.createElement("tr");
+    const tdName = document.createElement("td");
+    const tdEmail = document.createElement("td");
+    const tdAddress = document.createElement("td");
+    const tdPhone = document.createElement("td");
+    const tdCheck = document.createElement("td");
+    const inputCheck = document.createElement("input");
 
-    td.innerText = name;
-    console.log(td);
-    tBody.appendChild(td);
+    inputCheck.className = "checkbox";
+    inputCheck.type = "checkbox";
+    tdCheck.appendChild(inputCheck);
+    tdName.innerText = name;
+    tdEmail.innerText = email;
+    tdAddress.innerText = address;
+    tdPhone.innerText = phone;
+    //console.log(td);
+    //console.log(name);
+    tr.appendChild(tdCheck);
+    tr.appendChild(tdName);
+    tr.appendChild(tdEmail);
+    tr.appendChild(tdAddress);
+    tr.appendChild(tdPhone);
+    tBody.appendChild(tr);
+}
 
+const loadApi = async () => {
+    /*    try {
+        const inputRaza = document.querySelector("#busq").value;
+        const respApiRaza = await axios.get(`https://api.thecatapi.com/v1/breeds/search?q=${inputRaza}`);
+        addRowCat_table(respApiRaza.data[0]);
+        activateCat_table();
+
+    } catch(err) {
+        console.log("ERROR", err)
+    }*/
+    try {
+        const api = await axios.get("https://5f7c70d600bd74001690ac5e.mockapi.io/users")
+        console.log("la respuesta es:", api.data);
+        console.log("una fila de la respuesta es:", api.data[0]);
+        api.data.forEach(item => {
+            console.log("El nombre es", item.fullname);
+            //showDataTable(item.fullname);  
+            showDataTable(item.fullname, item.email, item.address, item.phone);
+        })
+    } catch(err) {
+        console.log("ERROR", err);
+    }   
 }
 
 
-
 const load = () => {
-    const nombre = ["Stefania"];
-    showDataTable(nombre);
-    console.log(`hola nueva web! @mirinnes`);
-
-
+    loadApi();
 }
