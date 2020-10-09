edc0bb42345cb3a186739045357e302d1379d18f
Miranda Lopez <mirandaineslopez@gmail.com>
2020-10-10 01:18:40 +0200

    aporte 3

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
+/*
+4. Al hacer click en el botón Nuevo, mostrar en un modal 
+un formulario con los siguientes datos y un botón de guardar:
+
+    Nombre (Máximo 50 carácteres)
+    Dirección (Máximo 60 carácteres)
+    Telefono (Solo números, espacios y/o guiones medios. 
+        Validar el formato utilizando expresiones regulares)
+    Email (Validar el formato utilizando expresiones regulares)
+
+Al hacer click en el botón guardar, primero se debe validar 
+que los campos sean válidos. En caso afirmativo, 
+hacer un POST a la ruta /users enviando el objeto con las 4 propiedades 
+(fullname, email, address, phone).
+
+
+6.Cada elemento en la tabla tendrá la acción editar. 
+    Al hacer click en editar, mostrar en un modal un formulario 
+    con los mismos campos (y validaciones) que el fomulario nuevo, 
+    con todos los datos pre-cargados. Al finalizar la edición, 
+    debemos validar que todos los datos ingresados sean correctos 
+    (al igual que en la creación). Si los datos son correctos, 
+    haremos un PUT a la ruta /users/:id.
+*/
+
+
+
+
+
+
+
+// / / / / / / / / / / / / / / / / /
+const vaciarTabla = () => {
+    const trALL = document.querySelectorAll("#main-tabla_tbody > tr");
+    console.log("Los hijos de main_tabla son:", trALL);
+    trALL.forEach(tr=>tr.remove());
+}
+
 const showDataTable = (name, email, address, phone) => {
+
     const tBody = document.querySelector("#main-tabla_tbody");
     const tr = document.createElement("tr");
     const tdName = document.createElement("td");
@@ -7,7 +46,16 @@ const showDataTable = (name, email, address, phone) => {
     const tdPhone = document.createElement("td");
     const tdCheck = document.createElement("td");
     const inputCheck = document.createElement("input");
+    const tdDelete = document.createElement("td");
+    const tdEditar = document.createElement("td");
+    const btnDelete = document.createElement("button");
+    const btnEditar = document.createElement("button");
 
+
+    btnDelete.innerHTML = `<i class="material-icons" title="Delete">&#xE872;</i>`;
+    btnEditar.innerHTML = `<i class="material-icons" title="Edit">&#xE254;</i>`;
+    btnDelete.className = "btnDelete";
+    btnEditar.className = "btnEditar";
     inputCheck.className = "checkbox";
     inputCheck.type = "checkbox";
     tdCheck.appendChild(inputCheck);
@@ -15,41 +63,120 @@ const showDataTable = (name, email, address, phone) => {
     tdEmail.innerText = email;
     tdAddress.innerText = address;
     tdPhone.innerText = phone;
-    //console.log(td);
-    //console.log(name);
+
+    tdDelete.appendChild(btnDelete);
+    tdEditar.appendChild(btnEditar);
     tr.appendChild(tdCheck);
     tr.appendChild(tdName);
     tr.appendChild(tdEmail);
     tr.appendChild(tdAddress);
     tr.appendChild(tdPhone);
+    tr.appendChild(tdEditar);
+    tr.appendChild(tdDelete);
     tBody.appendChild(tr);
 }
 
 const loadApi = async () => {
-    /*    try {
-        const inputRaza = document.querySelector("#busq").value;
-        const respApiRaza = await axios.get(`https://api.thecatapi.com/v1/breeds/search?q=${inputRaza}`);
-        addRowCat_table(respApiRaza.data[0]);
-        activateCat_table();
-
-    } catch(err) {
-        console.log("ERROR", err)
-    }*/
     try {
         const api = await axios.get("https://5f7c70d600bd74001690ac5e.mockapi.io/users")
-        console.log("la respuesta es:", api.data);
-        console.log("una fila de la respuesta es:", api.data[0]);
+        console.log("lo que hay en api.data es", api.data);
         api.data.forEach(item => {
-            console.log("El nombre es", item.fullname);
-            //showDataTable(item.fullname);  
             showDataTable(item.fullname, item.email, item.address, item.phone);
         })
     } catch(err) {
         console.log("ERROR", err);
-    }   
+    }
 }
 
 
 const load = () => {
+    const inputSearch = document.querySelector("#search");
+
+
+    console.log("el inputsearch es:", inputSearch);
+
+    inputSearch.addEventListener("keypress",(e) => {
+        if (e.key === 'Enter'){
+            search();
+        }
+    });
+
     loadApi();
 }
+
+//Filtrar:
+const search = async () => {
+    console.log("LLamaste al search!");
+    const inputSearch = document.querySelector("#search").value;
+    console.log(`El valor es: ${inputSearch}`);
+    //TODO: validar(inputSearch) 
+    vaciarTabla();
+
+    try {
+        const api = await axios.get("https://5f7c70d600bd74001690ac5e.mockapi.io/users")
+        const resBusqueda = api.data.filter(item => item.fullname == inputSearch || item.email == inputSearch || item.address == inputSearch || item.phone == inputSearch);
+        console.log("El resBusqueda es", resBusqueda);
+        showDataTable(resBusqueda[0].fullname, resBusqueda[0].email, resBusqueda[0].address, resBusqueda[0].phone)
+    } catch(err) {
+        console.log("ERROR", err);
+    }
+}
+
+
+
+
+/*const search = async() => {
+    const inputSearch = document.querySelector("#search");
+
+    try {
+        const response = await axios.get(`https://api.thecatapi.com/v1/breeds/search?q=${inputSearch.value}`)
+        const gatitos = response.data;
+
+        const html = gatitos.reduce((html,gatito)=>{
+            return html+`
+            <tr>
+             <td>${gatito.name}</td>
+             <td>${gatito.weight.metric}</td>
+             <td>${gatito.origin}</td>
+             <td>${gatito.temperament}</td>
+             <td>${gatito.description}</td>
+            </tr>
+            `
+        },"")
+
+        document.querySelector("#table-search").innerHTML=`<table>
+        <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Peso</th>
+          <th>Origen</th>
+          <th>Temperamento</th>
+          <th>Descripcion</th>
+        </tr>
+        </thead>
+        <tbody>
+        ${html}
+        </tbody>
+        </table>`;
+
+        console.log({gatitos,html});
+    } catch (error) {
+        console.log(error)
+    }
+
+    let tableHtml = `<table>
+    <thead>
+    <tr>
+      <th>Nombre</th>
+      <th>Peso</th>
+      <th>Origen</th>
+      <th>Temperamento</th>
+      <th>Descripcion</th>
+    </tr>
+    </thead>`
+    
+    
+
+  
+   
+}*/
